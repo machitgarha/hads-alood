b42d30e7e67fd0bac53adb69fd1392b0885cacb4
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-01-24 16:18:42 +0330

    Add yet-incomplete architecture for comparator_n_bit
    Use comparator_1_bit inside it to compare all bits one by one, by
    using a generator.

--- src/comparator-n-bit.vhd
+++ src/comparator-n-bit.vhd
@@ -15,3 +15,32 @@ entity comparator_n_bit is
         lt, eq, gt: out std_logic
     );
 end entity;
+
+architecture structural of comparator_n_bit is
+    component comparator_1_bit is
+        port(
+            left_operand, right_operand: in std_logic;
+
+            -- Just like a decoder, exactly one of the followings will be 1.
+            -- lt: Lower-than
+            -- eq: Equals
+            -- gt: Greater-than
+            lt, eq, gt: out std_logic
+        );
+    end component;
+
+    signal lt_bits, eq_bits, gt_bits: std_logic_vector(n - 1 downto 0);
+begin
+    l_compare_operand_bits:
+    for i in 0 to n - 1 generate
+        comparator_1_bit_i: comparator_1_bit port map(
+            left_operand => left_operand(i),
+            right_operand => right_operand(i),
+
+            lt => lt_bits(i),
+            eq => eq_bits(i),
+            gt => gt_bits(i)
+        );
+    end generate;
+
+end architecture;
