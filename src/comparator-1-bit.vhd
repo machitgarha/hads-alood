84632fb3dad8176f2a9a853763e4ec3da29e228f
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-01-24 11:55:47 +0330

    Rename some IO signals in comparator_{1,n}_bit entities
    left_operand and right_operand are more informative than in_1 and
    in_2.
    
    Also, sending an encoded result to the output might be bad idea,
    while reducing output signals from 3 to 2. Simply because an
    encoded output needs a decoder.

--- src/comparator-1-bit.vhd
+++ src/comparator-1-bit.vhd
@@ -3,11 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity comparator_1_bit is
     port(
-        in_1, in_2: in std_logic;
+        left_operand, right_operand: in std_logic;
 
         -- Just like a decoder, exactly one of the followings will be 1.
-        lt: out std_logic;
-        eq: out std_logic;
-        gt: out std_logic
+        -- lt: Lower-than
+        -- eq: Equals
+        -- gt: Greater-than
+        lt, eq, gt: out std_logic
     );
 end entity;
--- src/comparator-n-bit.vhd
+++ src/comparator-n-bit.vhd
@@ -3,17 +3,15 @@ use ieee.std_logic_1164.all;
 
 entity comparator_n_bit is
     generic(
-        constant n: integer;
-
-        -- Mappings of different comparison results into logic signals. This means, for
-        -- example, when the first number is lower than the second one, the output will be
-        -- the same as lt_out constant.
-        constant lt_out: std_logic_vector(1 downto 0) := "00";
-        constant eq_out: std_logic_vector(1 downto 0) := "01";
-        constant gt_out: std_logic_vector(1 downto 0) := "10"
+        constant n: integer
     );
     port(
-        x, y: in std_logic_vector(n - 1 downto 0);
-        result: out std_logic_vector(1 downto 0)
+        left_operand, right_operand: in std_logic_vector(n - 1 downto 0);
+
+        -- Just like a decoder, exactly one of the followings will be 1.
+        -- lt: Lower-than
+        -- eq: Equals
+        -- gt: Greater-than
+        lt, eq, gt: out std_logic
     );
 end entity;
