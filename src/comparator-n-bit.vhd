b7f1a5465f483b69a8136366a462956810a2631e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-01-24 20:27:08 +0330

    Implement lt based on eq and gt
    The reasons are just mentioned in code comments.

--- src/comparator-n-bit.vhd
+++ src/comparator-n-bit.vhd
@@ -33,6 +33,9 @@ architecture structural of comparator_n_bit is
     -- Signals used for saving results of smaller blocks of greater-than comparison
     -- operation, consisting of many and and or gates.
     signal gt_and_results, gt_or_results: std_logic_vector(n - 1 downto 0);
+
+    -- Temporary signals saving the eq and gt results, to be used in lower-than output.
+    signal eq_tmp, gt_tmp: std_logic;
 begin
     l_one_on_one_bits_comparison:
     for i in 0 to n - 1 generate
@@ -54,7 +57,7 @@ begin
         one_bits(i) <= '1';
     end generate;
 
-    eq <= '1' when eq_bits = one_bits else '0';
+    eq_tmp <= '1' when eq_bits = one_bits else '0';
 
     gt_or_results(0) <= gt_bits(0);
 
@@ -65,6 +68,14 @@ begin
         gt_or_results(i + 1) <= gt_and_results(i) or gt_bits(i + 1);
     end generate;
 
-    gt <= gt_or_results(n - 1);
+    gt_tmp <= gt_or_results(n - 1);
+
+    -- Lower-than is just the opposite of greater-than or equals. We could implement it
+    -- just like greater-than, but this way it avoids code duplication, making its chip
+    -- (i.e. physical block) smaller and the code more maintainable, while having a small
+    -- extra delay.
+    lt <= gt_tmp nor eq_tmp;
+    eq <= eq_tmp;
+    gt <= gt_tmp;
 
 end architecture;
