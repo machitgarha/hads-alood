101b6bd5a56e46da50f98d3cc94c92fb5f6798c0
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-01-25 15:05:12 +0330

    Implement latch_gated_d

--- src/latch-d.vhd
+++ src/latch-d.vhd
@@ -7,3 +7,16 @@ entity latch_gated_d is
         q, q_not: out std_logic
     );
 end entity;
+
+architecture structural of latch_gated_d is
+    -- As it is not possible to connect an output port to an internal gate, we need
+    -- separated connectors holding q and q_not values, connecting them to both internals
+    -- and actual output ports (i.e. q and q_not).
+    signal q_connector, q_not_connector: std_logic := '0';
+begin
+    q_connector <= (d nand clock) nand q_not_connector;
+    q_not_connector <= ((not d) nand clock) nand q_connector;
+
+    q <= q_connector;
+    q_not <= q_not_connector;
+end architecture;
