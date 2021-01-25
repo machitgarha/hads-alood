ae29cff39670fc22ed4614da6a6d0141c94a3028
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-01-25 15:34:51 +0330

    Rename flip_flop_master_slave_d -> flip_flop_neg_edge_d
    The name now represents its functionality instead of its actual
    implementation, which is better for understanding.

--- src/flip-flop-neg-edge-d.vhd
+++ src/flip-flop-neg-edge-d.vhd
@@ -1,14 +1,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity flip_flop_master_slave_d is
+-- Negative-edge-triggered D flip-flop
+entity flip_flop_neg_edge_d is
     port(
         d, clock: in std_logic;
         q, q_not: out std_logic
     );
 end entity;
 
-architecture structural of flip_flop_master_slave_d is
+architecture structural_master_slave of flip_flop_neg_edge_d is
     component latch_gated_d is
         port(
             d, clock: in std_logic;
