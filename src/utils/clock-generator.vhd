5e7b73ac0214500b7001b69814590ee9aeb8680d
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-01-26 17:16:49 +0330

    Fix a missing factor in clock_generator causing constant output

--- src/utils/clock-generator.vhd
+++ src/utils/clock-generator.vhd
@@ -18,8 +18,8 @@ architecture structural of clock_generator is
     signal clock_tmp: std_logic := '0';
 begin
     l_clock_generator:
-    for i in 0 to cycle_iterations - 1 generate
-        clock_tmp <= not clock_tmp after half_cycle_period;
+    for i in 1 to cycle_iterations generate
+        clock_tmp <= not clock_tmp after i * half_cycle_period;
     end generate;
 
     -- Everything is concurrent, so it can be outside of the generate loop above
