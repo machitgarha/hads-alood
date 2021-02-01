8a0283af5650d34edea351f23c55e10f5fb11975
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-01 19:59:12 +0330

    Change register_n_bit to include an asynchronous reset input port

--- src/registers/register-n-bit.vhd
+++ src/registers/register-n-bit.vhd
@@ -10,32 +10,22 @@ entity register_n_bit is
         clock: in std_logic;
         result: out std_logic_vector(n - 1 downto 0);
 
+        -- Resets the register to zero.
+        reset: in std_logic := '0';
         -- Enable turns the register on or off.
         enable: in std_logic := '1'
     );
 end entity;
 
-architecture structural of register_n_bit is
-    component flip_flop_d_positive_edge is
-        port(
-            d, clock: in std_logic;
-            q, q_not: out std_logic
-        );
-    end component;
-
-    -- Signal activated when both clock and enable inputs are active.
-    signal enable_clock: std_logic;
+architecture behavioral of register_n_bit is
 begin
-    enable_clock <= enable and clock;
-
-    l_initialize_flip_flops:
-    for i in 0 to n - 1 generate
-        flip_flop_instance: flip_flop_d_positive_edge port map(
-            d => data(i),
-            clock => enable_clock,
-            q => result(i),
-            q_not => open
-        );
-    end generate;
-
+    process
+    begin
+        if reset = '1' then
+            -- Make all bits zero
+            result <= (others => '0');
+        elsif enable = '1' and rising_edge(clock) then
+            result <= data;
+        end if;
+    end process;
 end architecture;
