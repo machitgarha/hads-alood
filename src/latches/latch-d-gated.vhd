13b0475716e692e530443d5e7cdaa7d1926c250d
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-18 07:45:40 +0330

    Improve latch_d_gated by removing two extra middleware signals

--- src/latches/latch-d-gated.vhd
+++ src/latches/latch-d-gated.vhd
@@ -4,20 +4,14 @@ use ieee.std_logic_1164.all;
 entity latch_d_gated is
     port(
         d, clock: in std_logic;
-        q, q_not: out std_logic
+
+        q: out std_logic := '0';
+        q_not: out std_logic := '1'
     );
 end entity;
 
 architecture structural of latch_d_gated is
-    -- As it is not possible to connect an output port to an internal gate, we need
-    -- separated connectors holding q and q_not values, connecting them to both internals
-    -- and actual output ports (i.e. q and q_not).
-    signal q_connector: std_logic := '0';
-    signal q_not_connector: std_logic := '1';
 begin
-    q_connector <= d when clock = '1';
-    q_not_connector <= not d when clock = '1';
-
-    q <= q_connector;
-    q_not <= q_not_connector;
+    q <= d when clock = '1';
+    q_not <= not d when clock = '1';
 end architecture;
