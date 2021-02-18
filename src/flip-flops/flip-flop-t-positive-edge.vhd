2d149e55856b9015510c2646361c0ec2588dfb64
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-18 18:27:11 +0330

    Add clear capability to flip_flop_t_positive_edge

--- src/flip-flops/flip-flop-t-positive-edge.vhd
+++ src/flip-flops/flip-flop-t-positive-edge.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_1164.all;
 entity flip_flop_t_positive_edge is
     port(
         t, clock: in std_logic;
+        clear: in std_logic := '0';
         q: buffer std_logic := '0';
         q_not: buffer std_logic := '1'
     );
@@ -11,9 +12,12 @@ end entity;
 
 architecture behavioral of flip_flop_t_positive_edge is
 begin
-    process(clock)
+    process(clear, clock)
     begin
-        if rising_edge(clock) and t = '1' then
+        if clear = '1' then
+            q <= '0';
+            q_not <= '1';
+        elsif rising_edge(clock) and t = '1' then
             q <= q_not;
             q_not <= q;
         end if;
