2ab2e2edaa4fe9e5aaf4a662d99205cd863c6dcb
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-21 19:08:01 +0330

    Generalize random_generator_1_bit to random_generator_n_bit
    Use shifting to left and putting the new random bit in its LSB.

--- src/random/random-generator-n-bit.vhd
+++ src/random/random-generator-n-bit.vhd
@@ -1,19 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity random_generator_1_bit is
+entity random_generator_n_bit is
     generic(
+        -- Result size, i.e. number of random output bits.
+        constant n: natural range 1 to natural'high;
+
         -- The size of the input seed.
         constant seed_size: natural range 3 to natural'high := 5
     );
     port(
         clock: in std_logic;
         seed: in std_logic_vector(0 to seed_size - 1);
-        result: out std_logic := '0'
+
+        result: out std_logic_vector(n - 1 downto 0) := (others => '0');
+        done: out std_logic := '0'
     );
 end entity;
 
-architecture structural of random_generator_1_bit is
+architecture structural of random_generator_n_bit is
     type integer_array is array(natural range <>) of integer;
 
     -- The period to wait until clock reaches a stable state.
@@ -31,6 +36,7 @@ architecture structural of random_generator_1_bit is
     ) := (1, 1, 2, others => 0);
 
     signal seed_tmp: std_logic_vector(0 to seed_size - 1) := seed;
+    signal result_tmp: std_logic_vector(n - 1 downto 0) := (others => '0');
 begin
     process
     begin
@@ -43,17 +49,35 @@ begin
     -- The distribution of random bit generation might not be diverse well, but the
     -- approximate results shows its diversity is acceptable.
     process
+        -- Used to set 'done' port. It takes some time to fully generate n random bits,
+        -- so this variable takes care of this.
+        variable clock_cylces: natural := 0;
+
+        -- Count is a dynamic index to change the algorithm in the case of repetition.
         variable count: natural := 0;
         variable count_limit: natural := 0;
-        variable result_next_state: std_logic := '0';
+
+        variable result_lsb_tmp: std_logic := '0';
     begin
         wait until clock'event;
         wait for clock_stability_wait_period * 2;
 
-        result_next_state := seed_tmp(prev_selector(selector_range_end)) xor clock;
+        -- Managing 'done' bit
+        clock_cylces := clock_cylces + 1;
+        if clock_cylces >= n then
+            done <= '1';
+        else
+        end if;
 
-        result <= result_next_state;
-        seed_tmp(prev_selector(selector_range_start)) <= result_next_state;
+        -- Shift result to left by one
+        for i in n - 1 downto 1 loop
+            result_tmp(i) <= result_tmp(i - 1);
+        end loop;
+
+        result_lsb_tmp := seed_tmp(prev_selector(selector_range_end)) xor clock;
+
+        result_tmp(0) <= result_lsb_tmp;
+        seed_tmp(prev_selector(selector_range_start)) <= result_lsb_tmp;
 
         -- E.g. (a, b, c) -> (b, c, c)
         -- E.g. (a, b, c, d) -> (b, c, d, d)
@@ -68,7 +92,8 @@ begin
                 selector(i);
         end loop;
 
-        -- This must be seed and not seed_tmp
+        -- This must be seed and not seed_tmp. Changing it to seed_tmp leads to more
+        -- predictable results.
         if seed(count mod seed_size) = '1' then
             selector(selector_range_end) := selector(selector_range_end) + 1;
         end if;
@@ -76,7 +101,6 @@ begin
         -- Making sure the selector(s) do(es) not exceed seed bound
         selector(selector_range_end) := selector(selector_range_end) mod seed_size;
 
-        -- Count is an index to change the algorithm in the case of repetition.
         count := count + 1;
 
         -- Generate a random count_limit
@@ -106,4 +130,6 @@ begin
             count := selector_range_end;
         end if;
     end process;
+
+    result <= result_tmp;
 end architecture;
--- tests/unit/random/test-random-generator-1-bit.vhd
+++ tests/unit/random/test-random-generator-1-bit.vhd
@@ -5,14 +5,16 @@ entity test_random_generator_1_bit is
 end entity;
 
 architecture structural of test_random_generator_1_bit is
-    component random_generator_1_bit is
+    component random_generator_n_bit is
         generic(
+            constant n: natural range 1 to natural'high;
             constant seed_size: natural range 3 to natural'high := 5
         );
         port(
             clock: in std_logic;
             seed: in std_logic_vector(0 to seed_size - 1);
-            result: out std_logic := '0'
+            result: out std_logic_vector(n - 1 downto 0) := (others => '0');
+            done: out std_logic := '0'
         );
     end component;
 
@@ -26,11 +28,16 @@ architecture structural of test_random_generator_1_bit is
         );
     end component;
 
+    constant n: natural := 1;
     constant seed_size: natural := 4;
     constant seed_array: std_logic_vector(0 to 1) := ('0', '1');
 
+    type std_logic_vector_2d is array(natural range <>) of
+        std_logic_vector(n - 1 downto 0);
+
     signal clock: std_logic;
-    signal results: std_logic_vector(0 to 15);
+    signal done_bits: std_logic_vector(0 to 15);
+    signal results: std_logic_vector_2d(0 to 15);
 begin
     clock_generator_instance: clock_generator generic map(200, 1 ns) port map(clock);
 
@@ -42,9 +49,10 @@ begin
             for k in seed_array'range generate
                 l_seed_genenerator:
                 for l in seed_array'range generate
-                    instance: random_generator_1_bit generic map(seed_size) port map(
+                    instance: random_generator_n_bit generic map(n, seed_size) port map(
                         clock,
-                        (seed_array(i), seed_array(j), seed_array(k), seed_array(l)), results(1 * i + 2 * j + 4 * k + 8 * l)
+                        (seed_array(i), seed_array(j), seed_array(k), seed_array(l)), results(1 * i + 2 * j + 4 * k + 8 * l),
+                        done_bits(1 * i + 2 * j + 4 * k + 8 * l)
                     );
                 end generate;
             end generate;
