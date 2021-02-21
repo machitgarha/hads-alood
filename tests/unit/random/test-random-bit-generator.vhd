c01e49058e926084a7eb85a476379db84a1e9c7f
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-21 13:59:09 +0330

    Make test_random_bit_generator to test againts all possible 4-bit seeds
    To see whether the generated bit is random or not, how they are
    related to seed, and if two seeds produce the same result or not.

--- tests/unit/random/test-random-bit-generator.vhd
+++ tests/unit/random/test-random-bit-generator.vhd
@@ -27,13 +27,27 @@ architecture structural of test_random_bit_generator is
     end component;
 
     constant n: natural := 4;
+    constant seed_array: std_logic_vector(0 to 1) := ('0', '1');
 
     signal clock: std_logic;
-    signal result: std_logic;
-
-    signal seed: std_logic_vector(0 to n - 1) := "0101";
+    signal results: std_logic_vector(0 to 15);
 begin
-    clock_generator_instance: clock_generator generic map(100, 1 ns) port map(clock);
+    clock_generator_instance: clock_generator generic map(200, 1 ns) port map(clock);
 
-    instance: random_bit_generator generic map(n) port map(clock, seed, result);
+    l_seed_genenerator:
+    for i in seed_array'range generate
+        l_seed_genenerator:
+        for j in seed_array'range generate
+            l_seed_genenerator:
+            for k in seed_array'range generate
+                l_seed_genenerator:
+                for l in seed_array'range generate
+                    instance: random_bit_generator generic map(n) port map(
+                        clock,
+                        (seed_array(i), seed_array(j), seed_array(k), seed_array(l)), results(1 * i + 2 * j + 4 * k + 8 * l)
+                    );
+                end generate;
+            end generate;
+        end generate;
+    end generate;
 end architecture;
