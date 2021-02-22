8075b2c9cc0f68cd1ec793559c4bc7bef6a79c6e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-22 03:38:04 +0330

    Add led_lock and enable ports to led_controller
    Also, disable led_{correct,upper,less} ports when enable is set to
    '0' (i.e. when led_lock is on). This makes one LEDs to be enabled
    at a time.

--- src/visual/led-controller.vhd
+++ src/visual/led-controller.vhd
@@ -3,14 +3,15 @@ use ieee.std_logic_1164.all;
 
 entity led_controller is
     port(
-        lt, eq, gt: in std_logic;
-        led_correct, led_upper, led_less: out std_logic
+        lt, eq, gt, enable: in std_logic;
+        led_correct, led_upper, led_less, led_lock: out std_logic
     );
 end entity;
 
 architecture structural of led_controller is
 begin
-    led_correct <= eq;
-    led_upper <= gt;
-    led_less <= lt;
+    led_correct <= eq and enable;
+    led_upper <= gt and enable;
+    led_less <= lt and enable;
+    led_lock <= not enable;
 end architecture;
