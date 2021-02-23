2d12948fff6f4c3d0a574ce53555df95e019996b
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-23 16:33:51 +0330

    Rename enable to locked, making led_controller a dummy entity

--- src/visual/led-controller.vhd
+++ src/visual/led-controller.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity led_controller is
     port(
-        lt, eq, gt, enable: in std_logic;
+        lt, eq, gt, locked: in std_logic;
         led_correct, led_upper, led_less, led_lock: out std_logic
     );
 end entity;
@@ -13,5 +13,5 @@ begin
     led_correct <= eq;
     led_upper <= gt;
     led_less <= lt;
-    led_lock <= not enable;
+    led_lock <= locked;
 end architecture;
