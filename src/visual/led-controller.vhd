582cd3b75b2b385e22f48995c7df9fd307fd6ee1
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-02-23 21:14:37 +0330

    Add enable port to led_controller (its why is explained as a comment)

--- src/visual/led-controller.vhd
+++ src/visual/led-controller.vhd
@@ -3,15 +3,17 @@ use ieee.std_logic_1164.all;
 
 entity led_controller is
     port(
-        lt, eq, gt, locked: in std_logic;
+        -- Enable is used when we want to hide LED results, e.g. because of an unstable
+        -- state, and turn all of them off.
+        lt, eq, gt, locked, enable: in std_logic;
         led_correct, led_upper, led_less, led_lock: out std_logic
     );
 end entity;
 
 architecture structural of led_controller is
 begin
-    led_correct <= eq;
-    led_upper <= gt;
-    led_less <= lt;
-    led_lock <= locked;
+    led_correct <= eq and enable;
+    led_upper <= gt and enable;
+    led_less <= lt and enable;
+    led_lock <= locked and enable;
 end architecture;
